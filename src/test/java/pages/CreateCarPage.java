d62eaf66adc66d61b73ef7a06156c3b182d79aa8
Ash0218 <ajykim218@gmail.com>
2020-01-06 17:13:43 -0500

    day 18 Vytrack automation

--- src/test/java/pages/CreateCarPage.java
+++ src/test/java/pages/CreateCarPage.java
@@ -2,11 +2,15 @@ package pages; // 120619
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import utils.BrowserUtils;
 import utils.Driver;
 
 public class CreateCarPage extends BasePage { // 1
     // extended BasePage
+
+    @FindBy(css = "[id^='custom_entity_type_LicensePlate']") // 10
+    public WebElement licensePlateElement; // 9
 
 
 // This method (#2-8) stands for selecting tags.
@@ -15,7 +19,9 @@ public class CreateCarPage extends BasePage { // 1
     public void selectTags(String tagName){ // 2
         String locator = "//label[text()='"+tagName+"']/preceding-sibling::input[@type='checkbox']"; // 3
         // tagName -> it can locate all different tag names such as Convertible,
-        //  Senior, or Junior, etc in the Website, Vytrack.com -> Vehicles -> Create Car
+        //  Senior, or Junior, etc in the Website, Vytrack.com -> Vehicles -> Create Car.
+        // locator for checkbox is based on label name. Label and checkbox
+        //  are siblings.
 
         WebElement checkBox = Driver.get().findElement(By.xpath(locator)); // 4
         BrowserUtils.waitForVisibility(checkBox, 15); // 5
