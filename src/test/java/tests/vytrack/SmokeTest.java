7f6b98dd50544338f37c3e94d5e68f268008a7ec
Ash0218 <ajykim218@gmail.com>
2020-01-06 13:22:51 -0500

    day 18 Vytrack automation

--- src/test/java/tests/vytrack/SmokeTest.java
+++ src/test/java/tests/vytrack/SmokeTest.java
@@ -1,10 +1,39 @@
 package tests.vytrack; // 120619
 
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.LoginPage;
 import tests.TestBase;
 
 public class SmokeTest extends TestBase { //1
     // Extended TestBase
+
+    @Test(dataProvider = "navigationInfo") // 10
+    public void smokeTest(String moduleName, String subModuleName, String pageSubTitle){ // 9
+        // putting the module names (#4,5,6,7,8) into #9 Test
+        // Run the test 4 times (for #5,6,7,8), so #9 has 3 variables
+        // (1,2,3), and #5,6,7,8 also have 3 variables with comma
+
+        extentTest = extentReports.createTest("Verify that page subtitle is equals to " + pageSubTitle); // 15
+        // report
+
+        LoginPage loginPage = new LoginPage(); // 11
+        // Create login page
+
+        loginPage.login("storemanager85", "UserUser123"); // 12
+        // login with username and password
+
+        loginPage.navigateTo(moduleName, subModuleName); // 13
+        // navigate into moduleName and subModuleNAme
+
+        Assert.assertEquals(loginPage.getPageSubTitle(), pageSubTitle); // 14
+        // Verify login page is the sub title.
+
+        extentTest.pass("Verified that page subtitle, ' " + pageSubTitle + " ' is displayed"); // 16
+
+
+    }
 
     @DataProvider(name = "navigationInfo") // 3
     public Object[][] navigationInfo(){ // 2
