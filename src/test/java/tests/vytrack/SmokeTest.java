c593fcfd7e1445c55ade2321ddc9aa71514234d5
Ash0218 <ajykim218@gmail.com>
2020-01-06 12:52:52 -0500

    day 18 Vytrack automation

---
+++ src/test/java/tests/vytrack/SmokeTest.java
@@ -0,0 +1,19 @@
+package tests.vytrack; // 120619
+
+import org.testng.annotations.DataProvider;
+import tests.TestBase;
+
+public class SmokeTest extends TestBase { //1
+    // Extended TestBase
+
+    @DataProvider(name = "navigationInfo") // 3
+    public Object[][] navigationInfo(){ // 2
+        return new Object[][]{ // 4
+                {"DashBoards", "Dashboard", "Dashboard"}, // 5
+                // DashBoards and Dashboard are from the Vytrack website
+                {"DashBoards", "Manage Dashboards", "All Manage Dashboards" } // 6
+                {"Fleet", "Vehicles", "All Cars"},
+                {"Customers"}
+        };
+    }
+}
